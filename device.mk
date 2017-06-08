1dbbfccc1dcc37d9390feef6274d05100ee3ee7d
Vachounet <vachounet@live.fr>
2017-06-08 09:25:23 +0200

    Revert "potter: build libjni_livedisplay"
    (reverted from commit 612006ae513417b9a6c8500958717c21c8ee555c)

--- device.mk
+++ device.mk
@@ -129,10 +129,6 @@ PRODUCT_PACKAGES += \
     memtrack.msm8953 \
     libtinyxml
 
-# Display Calibration
-PRODUCT_PACKAGES += \
-    libjni_livedisplay
-
 # DRM
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite
