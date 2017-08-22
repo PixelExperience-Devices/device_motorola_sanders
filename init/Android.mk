1eeea0ebdb055c02362507405d1beda5dcb4a25c
Vachounet <vachounet@live.fr>
2017-08-22 16:22:35 +0200

    potter: init: update for O
    Change-Id: I1668264ac5c6cbfb0d67f7a0f28775fac1d48304

--- init/Android.mk
+++ init/Android.mk
@@ -7,5 +7,7 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_CPPFLAGS := -Wall -DANDROID_TARGET=\"$(TARGET_BOARD_PLATFORM)\"
 LOCAL_SRC_FILES := init_potter.cpp
 LOCAL_MODULE := libinit_potter
+LOCAL_STATIC_LIBRARIES := \
+    libbase
 
 include $(BUILD_STATIC_LIBRARY)
