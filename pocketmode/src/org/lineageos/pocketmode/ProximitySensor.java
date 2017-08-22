cb11808477be30213b5cd5570775c49bd7d04a7d
myfluxi <linflux@arcor.de>
2017-08-22 16:22:25 +0200

    sanders: add pocketmode app
    Change-Id: I58b550dad0a71f3d9bd9865c00161f952512c898
    
    sanders: pocketmode: use org.lineageos.platform
    
    Change-Id: Ia7fe34c1a4eb221de8f09363583cf6b11c82fe96
    
    sanders: rebrand pocketmode app
    
    Change-Id: Ibdbcf047350356852022e443920769eafd7b4fe9
    
    sanders: pocketmode: Ensure FP is always left enabled when screen is turned on
    
    *) If you turn the screen on whilst the device is still in a pocket the
       fp sensor stays disabled even when the device is moved out into the open.
    
    *) This commit ensures that the FP sensor is always reenabled when the
       screen is turned on.
    
    Change-Id: If27967a468673f29d9c1a4742cc9dda95adaab59
    
    sanders: PocketMode: Explicitly include Android support libs
    
     * Fixes non-jack build
    
    Change-Id: I9d8346193577c7be218e12efb20f52ca9946eb14
    
    sanders: PocketMode: Properly depend on Lineage SDK
    
     * This is actually the proper library to include
       (only spotted while buidling with Jack disabled,
       because app compilation was failing).
    
    Change-Id: Ib61f93704c6411a4cb1f91d9e8693a1775934037
    
    sanders: PocketMode: Set LOCAL_PRIVATE_PLATFORM_APIS
    
    This change sets LOCAL_SDK_VERSION for all packages where
    this is possible without breaking the build, and
    LOCAL_PRIVATE_PLATFORM_APIS := true otherwise.
    
    Setting one of these two will be made required soon, and this
    is a change in preparation for that. Not setting LOCAL_SDK_VERSION
    makes the app implicitly depend on the bootclasspath, which is
    often not required. This change effectively makes depending on
    private apis opt-in rather than opt-out.
    
    Test: make relevant package
    Bug: 73535841
    Change-Id: I5196e8c7277ff86280782e7b479a499add2e8f9f
    
    pocketmode: Import local FileUtils class
    
    pocketmode: Rebrand to MotoPocketMode
    
    sanders: PocketMode: Fix FPC path

---
+++ pocketmode/src/org/lineageos/pocketmode/ProximitySensor.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.pocketmode;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class ProximitySensor implements SensorEventListener {
+
+    private static final boolean DEBUG = false;
+    private static final String TAG = "PocketModeProximity";
+
+    private static final String FPC_FILE = "/sys/devices/soc/7af8000.spi/spi_master/spi8/spi8.0/proximity_state";
+
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+    private Context mContext;
+
+    public ProximitySensor(Context context) {
+        mContext = context;
+        mSensorManager = (SensorManager)
+                mContext.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        setFPProximityState(event.values[0] < mSensor.getMaximumRange());
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        /* Empty */
+    }
+
+    private void setFPProximityState(boolean isNear) {
+        if (FileUtils.isFileWritable(FPC_FILE)) {
+            FileUtils.writeLine(FPC_FILE, isNear ? "1" : "0");
+        } else {
+            Log.e(TAG, "Proximity state file " + FPC_FILE + " is not writable!");
+        }
+    }
+
+    protected void enable() {
+        if (DEBUG) Log.d(TAG, "Enabling");
+        mSensorManager.registerListener(this, mSensor,
+                SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    protected void disable() {
+        if (DEBUG) Log.d(TAG, "Disabling");
+        mSensorManager.unregisterListener(this, mSensor);
+        // Ensure FP is left enabled
+        setFPProximityState(/* isNear */ false);
+    }
+}
