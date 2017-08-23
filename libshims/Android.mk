ca57723ea455943f7c85a6b314dd05dc3a954ab0
Vachounet <vachounet@live.fr>
2017-08-23 15:01:56 +0200

    potter: remove sensor shim for now
    Change-Id: I89cb51d4bec475fb89cdc63909d0208030001c85

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -40,14 +40,6 @@ LOCAL_MODULE := libshim_ims
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
-# RIL
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sensor.cpp
-LOCAL_SHARED_LIBRARIES := libgui
-LOCAL_MODULE := libshim_ril
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := get_process_name.c
