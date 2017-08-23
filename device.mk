01b09fc662da62de2f642befac8a9936fd266d94
Vachounet <vachounet@live.fr>
2017-08-23 12:55:56 +0200

    potter: Build the Bluetooth HAL
    Change-Id: Iba0b1a55ed91906a64727e53cad981791ab1fb59

--- device.mk
+++ device.mk
@@ -85,6 +85,10 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl
+
 # Browser
 PRODUCT_PACKAGES += \
     Gello
