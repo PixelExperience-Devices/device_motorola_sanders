cd74b30474fb5399638a2e5e5fb29cf333ca930a
Vachounet <vachounet@live.fr>
2017-08-23 13:05:22 +0200

    potter: Build USB HAL
    Change-Id: I0cad9dda736ca54fafbe48f08e4138d0903bb569

--- device.mk
+++ device.mk
@@ -312,6 +312,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-potter.conf:system/etc/thermal-engine-potter.conf
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
