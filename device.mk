6437c98d28b2500e2c1e7967ffc6eda9a5b95a61
LuK1337 <priv.luk@gmail.com>
2017-09-08 13:46:20 +0200

    potter: add basic 2.0 -> 2.1 fingerprint HAL wrapper
    Change-Id: I27963bfb1402aba10e4ceba669520b630d075abf

--- device.mk
+++ device.mk
@@ -153,7 +153,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-service \
+    fingerprint.msm8953
 
 # FM
 PRODUCT_PACKAGES += \
