15edf0f1372f860d630a1cfa414280f48191f8a7
Vachounet <vachounet@live.fr>
2017-09-20 13:45:34 +0200

    sanders: update BT device name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -20,7 +20,7 @@
 #include <cutils/properties.h>
 #include <string.h>
 
-#define BTM_DEF_LOCAL_NAME "Moto G5 Plus"
+#define BTM_DEF_LOCAL_NAME "Moto G5S Plus"
 #define BLUETOOTH_QTI_SW TRUE
 #define MAX_ACL_CONNECTIONS   16
 #define MAX_L2CAP_CHANNELS    16
