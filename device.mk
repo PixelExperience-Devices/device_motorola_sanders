7c7e3b402e8c1513088d05a1418126ed4f6aa685
Vachounet <vachounet@live.fr>
2017-09-20 13:48:57 +0200

    sanders: add thermal config

--- device.mk
+++ device.mk
@@ -330,7 +330,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-potter.conf:system/etc/thermal-engine-potter.conf
+    $(LOCAL_PATH)/configs/thermal-engine-sanders.conf:system/etc/thermal-engine-sanders.conf
 
 # USB HAL
 PRODUCT_PACKAGES += \
