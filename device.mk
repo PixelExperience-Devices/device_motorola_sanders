6512c831c4d4dd551d65211b245b27989175401c
Demon000 <demonsingur@gmail.com>
2017-09-24 16:30:56 +0200

    sanders: Convert lights HAL to native binderized HAL
    [Adapted from libra for griffin]
    
    Signed-off-by: Erfan Abdi <erfangplus@gmail.com>
    Signed-off-by: therootlord <igor_cestari@hotmail.com>

--- device.mk
+++ device.mk
@@ -243,8 +243,7 @@ PRODUCT_PACKAGES += \
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service \
-    lights.msm8953
+    android.hardware.light@2.0-service.sanders
 
 # Media
 PRODUCT_PACKAGES += \
