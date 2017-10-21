f8de10703366861d7dd6b6ec29882f99a27cf595
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-10-21 18:33:12 -0300

    sanders: Drop CMHW for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -106,10 +106,8 @@ BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_NO_CHARGER_LED := true
 
-# CMHW
-BOARD_USES_CYANOGEN_HARDWARE := true
+# DT2W
 TARGET_TAP_TO_WAKE_NODE := "/sys/android_touch/doubletap2wake"
-BOARD_HARDWARE_CLASS += $(DEVICE_PATH)/cmhw
 
 # CNE / DPM
 BOARD_USES_QCNE := true
