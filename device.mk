c7be3df0f6ac4c45f22978c131c600023078cc81
Vachounet <vachounet@live.fr>
2017-11-22 20:44:36 +0100

    sanders: build Camera2 for now

--- device.mk
+++ device.mk
@@ -101,7 +101,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     libshim_camera \
-    Snap \
+    Camera2 \
+    camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
     libshim_camera_hal \
