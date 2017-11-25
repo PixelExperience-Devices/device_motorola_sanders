29ee22beb0c0feabf347e5ffb14392f06b6f6a8d
Vachounet <vachounet@live.fr>
2017-11-25 10:38:27 +0100

    sanders: revert some binderized services

--- device.mk
+++ device.mk
@@ -137,11 +137,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libgenlock \
@@ -151,8 +148,7 @@ PRODUCT_PACKAGES += android.hardware.media.omx@1.0-impl
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl \
-    android.hardware.renderscript@1.0-service
+    android.hardware.renderscript@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -235,10 +231,6 @@ PRODUCT_PACKAGES += \
 # Display Calibration
 PRODUCT_PACKAGES += \
     libjni_livedisplay
-
-PRODUCT_PACKAGES += \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl
 
 # DRM
 PRODUCT_PACKAGES += \
