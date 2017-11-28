1af861c1bc809f3cb081d147dc0488521d99d140
Bindu Gokul <bgokul@codeaurora.org>
2017-11-28 16:03:58 +0530

    sanders: remove android.hardware.usb; no usbpd support
    Change-Id: Icb46be06afd4f23252439e481e36741cde346612

--- device.mk
+++ device.mk
@@ -388,10 +388,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
