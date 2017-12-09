9163181afbe0dfea0c015b4bbbae76b5b99827c0
Vachounet <vachounet@live.fr>
2017-12-09 10:23:23 +0100

    sanders: add perfboostconfig.xml

--- device.mk
+++ device.mk
@@ -318,7 +318,8 @@ PRODUCT_PACKAGES += \
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
-     $(LOCAL_PATH)/powerhint.xml:system/etc/powerhint.xml
+     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml \
+     $(LOCAL_PATH)/configs/perfboostsconfig.xml:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perfboostsconfig.xml
 
 # Releasetools script
 PRODUCT_COPY_FILES += \
