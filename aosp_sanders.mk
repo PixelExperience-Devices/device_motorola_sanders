cf3d03e6903a5e40ca047aeb24fb228beb1ec675
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-17 18:39:37 -0200

    sanders: Use Pixel build fingerprint to bypass safetynet

--- aosp_sanders.mk
+++ aosp_sanders.mk
@@ -34,6 +34,6 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT=motorola/sanders/sanders:7.1.1/NPS26.116-26/30:user/release-keys \
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys \
     PRIVATE_BUILD_DESC="sanders-7.1.1/NPS26.116-26/30:user/release-keys" \
     PRODUCT_NAME="Moto G5S Plus"
