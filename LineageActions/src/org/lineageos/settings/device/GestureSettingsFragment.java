a632d88af46924732bae17bba949a49996590fec
jhenrique09 <jhenrique09.mcz@hotmail.com>
2017-12-30 10:49:29 -0200

    sanders: LineageActions: Remove from dashboard, move to system settings
    Needs https://github.com/PixelExperience/packages_apps_Settings/commit/7ad487fd5c76dd368f372516ff7e65bf8123dc5c
    
    Signed-off-by: Keertesh <keertesh.k98@gmail.com>

--- LineageActions/src/org/lineageos/settings/device/GestureSettingsFragment.java
+++ LineageActions/src/org/lineageos/settings/device/GestureSettingsFragment.java
@@ -17,9 +17,17 @@
 package org.lineageos.settings.device;
 
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import android.support.v14.preference.PreferenceFragment;
+import android.support.v14.preference.SwitchPreference;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.content.Context;
+import android.content.res.Resources;
 
 public class GestureSettingsFragment extends PreferenceFragment {
+
+    private SwitchPreference mGestureDoubleTapPower;
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
@@ -30,6 +38,43 @@ public class GestureSettingsFragment extends PreferenceFragment {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.actions_panel);
+        mGestureDoubleTapPower = (SwitchPreference) findPreference("gesture_double_tap_power");
+        if (isCameraDoubleTapPowerAllowed()){
+            mGestureDoubleTapPower.setChecked(isCameraDoubleTapPowerEnabled());
+            mGestureDoubleTapPower.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object objValue) {
+                    boolean value = (Boolean) objValue;
+                    setCameraDoubleTapPowerEnabled(value);
+                    return true;
+                }
+            });
+            try {
+                Resources settingsRes = getActivity().createPackageContext("com.android.settings", 0).getResources();
+                mGestureDoubleTapPower.setTitle(settingsRes.getString(
+                        settingsRes.getIdentifier("double_tap_power_for_camera_title", "string", "com.android.settings")));
+                mGestureDoubleTapPower.setSummary(settingsRes.getString(
+                        settingsRes.getIdentifier("double_tap_power_for_camera_summary", "string", "com.android.settings")));
+            } catch (Exception e) {
+            }
+        }else{
+            getPreferenceScreen().removePreference(mGestureDoubleTapPower);
+        }
+    }
+
+    private boolean isCameraDoubleTapPowerAllowed() {
+        return getActivity().getResources().getBoolean(
+                com.android.internal.R.bool.config_cameraDoubleTapPowerGestureEnabled);
+    }
+
+    private boolean isCameraDoubleTapPowerEnabled(){
+        return Settings.Secure.getIntForUser(getActivity().getContentResolver(),
+                        Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, 1, UserHandle.USER_CURRENT) == 1;
+    }
+
+    private void setCameraDoubleTapPowerEnabled(boolean enabled){
+        Settings.Secure.putInt(getActivity().getContentResolver(),
+                Settings.Secure.CAMERA_DOUBLE_TAP_POWER_GESTURE_DISABLED, enabled ? 0 : 1);
     }
 
 }
