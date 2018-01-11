1ce89ffba6b20adb09fd57c6e93a64db1647efe0
therootlord <igor_cestari@hotmail.com>
2018-01-11 21:38:05 -0200

    sanders: remove Gello

--- device.mk
+++ device.mk
@@ -100,10 +100,6 @@ PRODUCT_PACKAGES += \
     libbt-vendor \
     android.hardware.bluetooth@1.0-impl
 
-# Browser
-PRODUCT_PACKAGES += \
-    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
