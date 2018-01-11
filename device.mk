449d402213c1ff6943f73290feb93054e2d707ba
Vachounet <vachounet@live.fr>
2018-01-11 13:57:17 +0100

    sanders: build configstore service

--- device.mk
+++ device.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
+    android.hardware.configstore@1.0-service \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libgenlock \
