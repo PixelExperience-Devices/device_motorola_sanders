4b10a523968f026b94d1a03bc48dbb26c18c482c
Vachounet <vachounet@live.fr>
2018-01-11 15:06:14 +0100

    sanders: shims cleanup
    Change-Id: Ideafbadf8d51c94b7505201239e32435dc3ea54f

--- device.mk
+++ device.mk
@@ -202,10 +202,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@1.0-impl \
     android.hardware.health@1.0-service
 
-# IMS
-PRODUCT_PACKAGES += \
-    libshim_ims
-
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
@@ -333,7 +329,6 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
-    libshim_ril \
     libprotobuf-cpp-full \
     libxml2
 
