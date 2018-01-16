8b11e4a70b98989fc7e51455fc123201bff912b2
Vachounet <vachounet@live.fr>
2018-01-16 17:30:20 +0100

    sanders: update post-install script
    * Permissions moved to vendor
    * Fix deep sleep issue on variants without NFC
    
    Change-Id: Ie5a24a3debe638a3f491d12aa62bf1775b955972

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -10,16 +10,16 @@ sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1806" ]; then
     # XT1806 doesn't have NFC chip
-    rm /system/etc/permissions/android.hardware.nfc.xml
-    rm /system/etc/permissions/android.hardware.nfc.hce.xml
-    rm /system/etc/permissions/com.android.nfc_extras.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 fi
 
 if ! [ "$sku" = "XT1802" ]; then
     # Others variants doesn't have DTV support
-    rm /system/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/etc/permissions/mot_dtv_permissions.xml
+    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
     rm /system/lib/libdtvhal.so
     rm /system/lib/libdtvtuner.so
     rm /system/lib64/libdtvtuner.so
