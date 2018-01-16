f494fbc72d7d8c3a325e47ba6c2a705c5db05cd9
Vachounet <vachounet@live.fr>
2018-01-16 17:30:20 +0100

    sanders: update post-install script
    * Permissions moved to vendor
    * Fix deep sleep issue on variants without NFC
    
    Change-Id: Ie5a24a3debe638a3f491d12aa62bf1775b955972

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -4,11 +4,11 @@ sku=`getprop ro.boot.hardware.sku`
 
 if [ "$sku" = "XT1687" ]; then
     # XT1687 doesn't have NFC chip
-    rm /system/etc/permissions/android.hardware.nfc.xml
-    rm /system/etc/permissions/android.hardware.nfc.hce.xml
-    rm /system/etc/permissions/com.android.nfc_extras.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 else
     # Only XT1687 variant got a compass
-    rm /system/etc/permissions/android.hardware.sensor.compass.xml
+    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
