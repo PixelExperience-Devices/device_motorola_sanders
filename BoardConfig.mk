496570a51fd0ed366a7e4d13f81e1d2441d4faac
Vachounet <vachounet@live.fr>
2018-02-19 10:14:18 +0100

    sanders: shim: transition to TARGET_LD_SHIM_LIBS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -214,6 +214,14 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 # Sensor
 USE_SENSOR_MULTI_HAL := true
 
+# Shim
+TARGET_LD_SHIM_LIBS := \
+    /system/vendor/bin/adspd|libshim_adsp.so \
+    /system/lib/lib_motsensorlistener.so|libsensor.so \
+	/system/lib/libjustshoot.so|libshims_camera.so \
+	/system/vendor/lib/libguy.so|libshim_camera_hal.so \
+	/system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
+
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_HAS_QCOM_WLAN_SDK          := true
