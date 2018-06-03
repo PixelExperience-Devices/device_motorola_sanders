59696a208c039a32cfae0eb894566b268db22e01
Vachounet <vachounet@live.fr>
2018-06-03 11:39:54 +0200

    sanders: build Snap
    * And enable api v2

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libbson \
     libgui_vendor \
+    Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-service \
