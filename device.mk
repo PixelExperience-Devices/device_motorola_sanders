b5fb157109e547ae8cd87c17dfa53e7b5fd33070
Michael Bestas <mkbestas@lineageos.org>
2018-06-11 13:09:04 +0200

    sanders: Build 1.0 power HAL
    * prima doesn't have support for wifi stats

--- device.mk
+++ device.mk
@@ -300,7 +300,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-service-qti
 
 # Qualcomm
 PRODUCT_COPY_FILES += \
