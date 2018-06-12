9c84b48a59eaa797fae4155148cad50a736a3171
Keertesh <keertesh.k98@gmail.com>
2018-06-12 12:27:17 +0530

    sanders: Update check_features.sh

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,14 +1,31 @@
 #!/sbin/sh
 
+# 1802 - Brazil
+# 1803 - Europe
+# 1804 - India
+# 1805 - Europe
+# 1806 - USA
+
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1687" ]; then
-    # XT1687 doesn't have NFC chip
-    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
-    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
-    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
+if [ "$sku" = "XT1806" ]; then
+    # XT1806 doesn't have NFC chip
+    rm /system/etc/permissions/android.hardware.nfc.xml
+    rm /system/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
-else
-    # Only XT1687 variant got a compass
-    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
+
+if ! [ "$sku" = "XT1802" ]; then
+    # Others variants doesn't have DTV support
+    rm /system/etc/permissions/com.motorola.hardware.dtv.xml
+    rm /system/etc/permissions/mot_dtv_permissions.xml
+    rm /system/lib/libdtvhal.so
+    rm /system/lib/libdtvtuner.so
+    rm /system/lib64/libdtvtuner.so
+    rm /system/vendor/lib/libdtvhal.so
+    rm /system/vendor/lib64/libdtvhal.so
+    rm -r /system/priv-app/DTVPlayer
+    rm -r /system/priv-app/DTVService
+fi
+
