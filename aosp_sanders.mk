c9d77c075b6bf5d62c18716dc0926f62c1c4d445
Keertesh <keertesh.k98@gmail.com>
2018-06-13 14:45:24 +0530

    derp

--- aosp_sanders.mk
+++ aosp_sanders.mk
@@ -22,7 +22,7 @@ $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 $(call inherit-product, vendor/aosp/common.mk)
 
 # Boot animation
-TARGET_BOOT_ANIMATIN_RES := 1080
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
