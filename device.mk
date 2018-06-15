3566d465e7cf0ee592544a5dc3bad0a7f20d24f4
therootlord <igor_cestari@hotmail.com>
2018-06-14 22:30:32 -0300

    sanders: build a 32b version of android.hardware.sensors service
    * Also add input group access

--- device.mk
+++ device.mk
@@ -360,7 +360,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
+    android.hardware.sensors@1.0-service_32 \
     sensorservice_32
 
 # Shims
