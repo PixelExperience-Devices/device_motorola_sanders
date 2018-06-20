bdd2ecae3e23e176dec29b83cbcc0a25f312df09
Dan Willemsen <dwillemsen@google.com>
2018-06-19 22:28:20 -0700

    sanders: build have overriding commands
    These products have warnings about overriding build commands, which
    means that there are multiple instructions on how to build a file, and
    we've just picked one of them. Which one we pick is usually stable, but
    it can change.
    
    Bug: 77611511
    Test: turn default to false, walleye still uses a warning
    Change-Id: I11d194863b313f64b19291f65219682b169d9ced

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -247,3 +247,5 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_P2P          := "p2p"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+
+BUILD_BROKEN_DUP_RULES := true
