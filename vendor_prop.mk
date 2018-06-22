ec2fc31ae9bf29fdbd6a97994a7907a4d870260e
elektroschmock <elektroschmock78@googlemail.com>
2018-06-22 12:58:16 +0200

    sanders: Run ActivityManager and RenderThread as SCHED_FIFO
    by setting sys.use_fifo_ui=1
    This is only usable on homogenous CPUs
    
    Reference:
    https://source.android.com/devices/tech/debug/jank_jitter
    
    Change-Id: I9793b45b631cf8b6db43219fe5d2a3a023ab64bf

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -296,3 +296,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.inputopts.enable=true \
     persist.vendor.qti.inputopts.movetouchslop=0.6
+
+# UI thread scheduling
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
