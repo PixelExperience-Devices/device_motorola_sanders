5ed8c5e43f1e125c06caf50e655041a16f840d01
therootlord <igor_cestari@hotmail.com>
2018-09-03 13:41:57 -0300

    sanders: update display props/manifest for P

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -116,7 +116,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.video.ubwc=0 \
     persist.hwc.enable_vds=1 \
     debug.sf.recomputecrop=0 \
-    debug.enable.sglscale=1
+    debug.enable.sglscale=1 \
+    vendor.display.enable_default_color_mode=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.display.disable_skip_validate=1
 
 # FM
 PRODUCT_PROPERTY_OVERRIDES += \
