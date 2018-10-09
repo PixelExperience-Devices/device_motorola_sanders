c70546d090678ac356cbee1c42e64f928fb06557
therootlord <igor_cestari@hotmail.com>
2018-10-09 14:35:16 -0300

    sanders: update check_features.sh script

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -2,13 +2,14 @@
 
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1687" ]; then
-    # XT1687 doesn't have NFC chip
+if [ "$sku" = "XT1806" ]; then
+    # XT1806 doesn't have NFC chip
     rm /vendor/etc/permissions/android.hardware.nfc.xml
     rm /vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /vendor/etc/permissions/android.hardware.nfc.hcef.xml
     rm /vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 else
-    # Only XT1687 variant got a compass
+    # Only XT1806 variant got a compass
     rm /vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
