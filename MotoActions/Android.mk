fd128400418e4e8a333c64ab6cd7764f367f4fe4
Alberto97 <albertop2197@gmail.com>
2018-11-17 12:36:50 +0100

    Actions: Clean up Android.mk

--- MotoActions/Android.mk
+++ MotoActions/Android.mk
@@ -11,22 +11,14 @@ LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_USE_AAPT2 := true
 
-LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v7-appcompat \
-    android-support-v7-preference \
-    android-support-v7-recyclerview \
-    android-support-v14-preference
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(LOCAL_PATH)/../../../../packages/resources/devicesettings/res
+    $(TOP)/packages/resources/devicesettings/res
 
 LOCAL_PROGUARD_ENABLED := disabled
 
 include frameworks/base/packages/SettingsLib/common.mk
 
 include $(BUILD_PACKAGE)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
