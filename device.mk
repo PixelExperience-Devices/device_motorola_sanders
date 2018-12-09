5ccea27ca9a65d6f1562cb1a3a6273cb9a4ca83b
therootlord <igor_cestari@hotmail.com>
2018-12-09 20:08:19 -0200

    sanders: use qcom power hal

--- device.mk
+++ device.mk
@@ -308,7 +308,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.0-impl \
+    android.hardware.power@1.0-service \
+    power.qcom
 
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
