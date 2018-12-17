f804e379a16d7f068d3028bb66c9ae202be8a392
Paul McLean <pmclean@google.com>
2018-12-17 08:14:18 -0700

    sanders: Adding Audio HAL V5: Direction API
    Change-Id: I4251b3853d00308dbd988625f8e7466a66b823fa
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- device.mk
+++ device.mk
@@ -76,9 +76,9 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES +=  \
