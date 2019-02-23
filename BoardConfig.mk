d4ee2324d97a64f39ad67bcc86430f9494779323
Nolen Johnson <johnsonnolen@gmail.com>
2019-02-23 04:38:06 +0000

    sanders: Remove deprecated TARGET_USES_64_BIT_BINDER
    Change-Id: Ibafc06659dae21e8acdd132bc2de7478008682b0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,7 +44,6 @@ TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
-TARGET_USES_64_BIT_BINDER := true
 
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := sanders,sanders_retail
