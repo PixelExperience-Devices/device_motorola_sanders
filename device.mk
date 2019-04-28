cec52bbbabf00e80de2361f726c347174e8633e1
Vaisakh Murali <vaisakhmurali@gmail.com>
2019-04-28 20:14:44 +0200

    sanders: audio: Add audio_tuning_mixer from OSS Hal
    * From CAF Tag LA.UM.7.6.r1-06400-89xx.0
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Change-Id: Ie32335b83f9203cadc2b02f6ff02b5f4a0612a4e

--- device.mk
+++ device.mk
@@ -84,6 +84,7 @@ PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
 PRODUCT_COPY_FILES +=  \
+    $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
