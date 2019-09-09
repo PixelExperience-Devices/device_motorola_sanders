e42efa0f4a639705c67ba70886942260360d43f2
Volodymyr Zhdanov <wight554@gmail.com>
2019-09-09 23:26:32 +0300

    sanders: Add packages for Android 10 IMS
    Change-Id: Ib387f4abcfc1895a420e05c4c87e7d00e65f02ba

--- device.mk
+++ device.mk
@@ -207,10 +207,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
 
-# IMS
+# IMS & Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common_system \
-    telephony-ext
+    ims_ext_common.xml \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
