1b558f3e65da2bbb116dbd4acc44408e7b1775b6
kubersharma001 <kubersharma001@gmail.com>
2019-10-11 18:42:00 +0530

    sanders: Add custom init zram size for all variants
    Change-Id: I0c5b428e305625d1abec522853299025de113026

--- init/init_sanders.cpp
+++ init/init_sanders.cpp
@@ -30,7 +30,7 @@
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-
+#include <sys/sysinfo.h>
 #include <android-base/properties.h>
 #include "property_service.h"
 #include "vendor_init.h"
@@ -54,6 +54,20 @@ void property_override_dual(char const system_prop[], char const vendor_prop[],
 {
     property_override(system_prop, value);
     property_override(vendor_prop, value);
+}
+
+/* Get Ram size for different variants */
+void check_device()
+{
+    struct sysinfo sys;
+    sysinfo(&sys);
+    if (sys.totalram > 3072ull * 1024 * 1024) {
+        property_set("ro.boot.ram", "4GB");
+    } else if (sys.totalram > 2048ull * 1024 * 1024) {
+        property_set("ro.boot.ram", "3GB");
+    } else {
+        property_set("ro.boot.ram", "2GB");
+    }
 }
 
 void num_sims() {
@@ -96,4 +110,6 @@ void vendor_load_properties()
     property_set("ro.hw.imager", "13MP");
 
     num_sims();
+
+    check_device();
 }
