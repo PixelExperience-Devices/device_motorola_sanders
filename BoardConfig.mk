8af3af758bb83b5655a332703017d1126d3a1303
Jarl-Penguin <jarlpenguin@outlook.com>
2019-10-28 22:46:25 +0300

    sanders: mke2fs is default on Pie
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,9 +237,6 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 PRODUCT_VENDOR_MOVE_ENABLED := true
 TARGET_COPY_OUT_VENDOR := vendor
 
-# Use mke2fs to create ext4 images
-TARGET_USES_MKE2FS := true
-
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_WLAN_DEVICE                := qcwcn
