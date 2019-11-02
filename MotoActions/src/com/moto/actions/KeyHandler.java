7944fa97e557057573da66fede55813617b530a8
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-11-02 03:30:47 +0100

    sanders: MotoActions: Migrate to AndroidX
    * also Adapt MotoActions for PixelExperience
    
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- MotoActions/src/com/moto/actions/KeyHandler.java
+++ MotoActions/src/com/moto/actions/KeyHandler.java
@@ -417,7 +417,7 @@ public class KeyHandler implements DeviceKeyHandler {
         return node;
     }
 
-    public boolean handleKeyEvent(KeyEvent event) {
+    public KeyEvent handleKeyEvent(KeyEvent event) {
         int scanCode = event.getScanCode();
 
         if (DEBUG) {
@@ -431,7 +431,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
         boolean isFPScanCode = ArrayUtils.contains(sSupportedFPGestures, scanCode);
         if (!isFPScanCode) {
-            return false;
+            return event;
         }
 
         boolean isFPGestureEnabled = FileUtils.readOneLine(FP_HOME_NODE).equals("1");
@@ -441,12 +441,12 @@ public class KeyHandler implements DeviceKeyHandler {
 
         // We only want ACTION_UP event
         if (event.getAction() != KeyEvent.ACTION_UP) {
-            return true;
+            return null;
         }
-        
+
         if (isFPScanCode){
             if (fpGesturePending) {
-                return false;
+                return event;
             } else {
                 resetFPGestureDelay();
                 fpGesturePending = true;
@@ -461,7 +461,7 @@ public class KeyHandler implements DeviceKeyHandler {
         if (isFPScanCode) {
             if ((!isFPGestureEnabled) || (!isScreenOn && !isFPGestureEnabledOnScreenOff)) {
                 resetDoubleTapOnFP();
-                return false;
+                return event;
             }
             if (!isScreenOn && isFPGestureEnabledOnScreenOff) {
                 processFPScreenOffScancode(scanCode);
@@ -469,7 +469,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 processFPScancode(scanCode);
             }
         }
-        return true;
+        return null;
     }
 
     private void processFPScancode(int scanCode) {
@@ -725,7 +725,7 @@ public class KeyHandler implements DeviceKeyHandler {
                     owningUid = android.os.Process.myUid();
                     owningPackage = mContext.getOpPackageName();
                     VibrationEffect effect = VibrationEffect.createOneShot(longpress ? 50 : 40, VibrationEffect.DEFAULT_AMPLITUDE);
-                    mVibrator.vibrate(owningUid, owningPackage, effect, VIBRATION_ATTRIBUTES);
+                    mVibrator.vibrate(owningUid, owningPackage, effect, null, VIBRATION_ATTRIBUTES);
                 }
             });
         }
