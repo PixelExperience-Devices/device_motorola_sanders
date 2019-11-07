a6cd321a4aff87767451416f3b4c782a85e186f6
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-11-07 23:41:44 +0100

    sanders: Fix script path in these files
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -24,9 +24,9 @@ VENDOR=motorola
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
-CM_ROOT="$MY_DIR"/../../..
+AOSP_ROOT="$MY_DIR"/../../..
 
-HELPER="$CM_ROOT"/vendor/cm/build/tools/extract_utils.sh
+HELPER="$AOSP_ROOT"/vendor/aosp/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
@@ -50,7 +50,7 @@ else
 fi
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$CM_ROOT"
+setup_vendor "$DEVICE" "$VENDOR" "$AOSP_ROOT"
 
 extract "$MY_DIR"/proprietary-files.txt "$SRC"
 
