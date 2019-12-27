e7944c1c1bf52b530466043ca47408de14b8a9e9
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-12-27 18:53:36 +0100

    sanders: Include ARCore
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- aosp_sanders.mk
+++ aosp_sanders.mk
@@ -22,6 +22,7 @@ $(call inherit-product, device/motorola/sanders/full_sanders.mk)
 TARGET_GAPPS_ARCH := arm64
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
