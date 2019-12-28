789ba887ec7865c3e233fc9cf5bee09935b40054
Jorge Lucas <jorgelucas.sd@gmail.com>
2019-12-28 04:14:57 +0100

    Revert "sanders: Fix Offline Charging"
    This reverts commit 77e3f30dae7f2add044415017e37daca39a44cc3.

--- device.mk
+++ device.mk
@@ -335,9 +335,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
-# Charger Images
-PRODUCT_PACKAGES += charger_res_images
-
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
