b4d61bb26c96d6658db5563a31317460dd16ec85
Jorg3Lucas <jorgelucas.sd@gmail.com>
2020-01-03 00:18:34 +0100

    sanders: switch to sepolicy-legacy-um
    Signed-off-by: Jorg3Lucas <jorgelucas.sd@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -224,7 +224,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
 SELINUX_IGNORE_NEVERALLOWS := true
