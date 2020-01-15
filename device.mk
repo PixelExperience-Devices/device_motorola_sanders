5e92ff53d159536020d02fa9f7f2ae7b7be6f095
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-01-15 03:31:29 +0100

    sanders: Import init.recovery.qcom.rc from CAF

--- device.mk
+++ device.mk
@@ -362,6 +362,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.mmi.boot.sh \
     init.mmi.rc \
+    init.recovery.qcom.rc \
     init.mmi.usb.rc \
     init.safailnet.rc \
     init.qcom.rc \
