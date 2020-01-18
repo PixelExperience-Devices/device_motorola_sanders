e04bce9ca5d2ec0b6f904957cd4973a9b39b324a
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-01-18 14:29:51 +0100

    sanders: Move Fingerprint to init
    * update build description
    * use coral fingerprint from Jan patch
    
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- init/init_sanders.cpp
+++ init/init_sanders.cpp
@@ -81,6 +81,10 @@ void vendor_load_properties()
     std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
     property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
 
+    // fingerprint
+    property_override("ro.build.description", "sanders-user 8.1.0 OPPS28.65-36-11-4 b6557 release-keys");
+    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+
     // rmt_storage
     std::string device = GetProperty("ro.boot.device", "");
     std::string radio = GetProperty("ro.boot.radio", "");
