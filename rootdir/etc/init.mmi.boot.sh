d2511a11dca9d74dc28debc266cc530c8b59f90f
JarlPenguin <jarlpenguin@outlook.com>
2020-01-28 19:56:19 +0300

    sanders: rootdir: insmod DTV module only for XT1802
    * Nuke ecompassd mentions because sensors.qti handles ecompass
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- rootdir/etc/init.mmi.boot.sh
+++ rootdir/etc/init.mmi.boot.sh
@@ -42,6 +42,3 @@ if [ -f /sys/devices/soc0/select_image ]; then
     echo $image_variant > /sys/devices/soc0/image_variant
     echo $oem_version > /sys/devices/soc0/image_crm_version
 fi
-
-enable ecompassd
-insmod /vendor/lib/modules/isdbt.ko
