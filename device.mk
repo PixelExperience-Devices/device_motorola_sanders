2690e1439330fd2c7e3bd57021a7291b712a144e
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-02-03 22:10:57 +0100

    sanders: Import init.recovery.qcom.rc from CAF
    * also update loop parts to 16
    
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- device.mk
+++ device.mk
@@ -352,6 +352,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.mmi.boot.sh \
     init.mmi.rc \
+    init.recovery.qcom.rc \
     init.mmi.usb.rc \
     init.qcom.rc \
     init.qcom.sensors.sh \
