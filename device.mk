cd5e3e3f44f1037cdff2e6ac27ad97361f3c2dc3
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-02-04 10:41:45 -0300

    sanders: Use Pixel Charger Animation

--- device.mk
+++ device.mk
@@ -326,8 +326,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
 # Charger Images
-PRODUCT_PACKAGES += charger_res_images
-
+PRODUCT_PACKAGES += \
+    product_charger_res_images
+    
 # Powerhint configuration file
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
