74e6c732659b1f59ba0b1c7367126436d5367d08
Jorge Lucas <jorgelucas.sd@gmail.com>
2020-02-27 16:16:00 +0100

    sanders: Allow neverallows
    Signed-off-by: Jorge Lucas <jorgelucas.sd@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,6 +227,7 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SELINUX_IGNORE_NEVERALLOWS := true
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
