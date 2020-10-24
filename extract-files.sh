f99a407d29718abde89c2de13aef422dc5e5f0e6
Michael Bestas <mkbestas@lineageos.org>
2020-10-24 10:08:46 -0400

    sanders: Switch to standalone extract utils
    * Align with templates while we are at it

--- extract-files.sh
+++ extract-files.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 #
-# Copyright (C) 2017-2019 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# If we're being sourced by the common script that we called,
-# stop right here. No need to go down the rabbit hole.
 
 function blob_fixup() {
     case "${1}" in
-
     vendor/lib/hw/camera.msm8953.so)
         sed -i "s|service.bootanim.exit|service.bootanim.hold|g" "${2}"
         ;;
@@ -40,21 +27,19 @@ function blob_fixup() {
     vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice)
         sed -i 's|/firmware/image|/vendor/f/image|' "${2}"
         ;;
-
-   esac
+    esac
 }
 
+# If we're being sourced by the common script that we called,
+# stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
     return
 fi
 
 set -e
 
-# Required!
 export DEVICE=sanders
 export DEVICE_COMMON=msm8953-common
 export VENDOR=motorola
-
-export DEVICE_BRINGUP_YEAR=2020
 
 "./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
